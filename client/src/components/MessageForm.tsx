7d0524476438c93497875fdb7df0e949756f0de3
Markus Johansen <markusraunjohansen@gmail.com>
2024-03-01 23:40:54 +0100

    Changes to chat view

--- client/src/components/MessageForm.tsx
+++ client/src/components/MessageForm.tsx
@@ -8,15 +8,25 @@ const MessageForm = ({ socket }: { socket: Socket }) => {
   const sendMessage = (e: FormEvent) => {
     e.preventDefault();
     socket.emit("send_message", { message: formValue, user: user });
+    setFormValue("");
   };
   return (
-    <form onSubmit={sendMessage}>
+    <form
+      onSubmit={sendMessage}
+      className="w-[40%] flex justify-between items-center"
+    >
       <input
+        className="bg-white text-black font-poppins text-sm border-none rounded-3xl h-full w-[87%] p-3"
         type="text"
         placeholder="Your Message"
         onChange={(e) => setFormValue(e.target.value)}
+        value={formValue}
       />
-      <input type="submit" value={"Send"} />
+      <input
+        className="p-2 bg-blue-600 rounded-3xl font-poppins text-white w-[10%] cursor-pointer active:bg-blue-700 border-2 border border-blue-600 hover:border-blue-800"
+        type="submit"
+        value={"Send"}
+      />
     </form>
   );
 };
